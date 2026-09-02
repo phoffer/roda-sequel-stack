f7aad9fd014ed08a8c0346757de3b154bfb88ff3
Paul Hoffer <git@paulhoffer.dev>
2026-09-01 18:53:40 -0700

    sequel timestamps plugin

--- models.rb
+++ models.rb
@@ -8,6 +8,7 @@ end
 
 Sequel::Model.plugin :auto_validations
 Sequel::Model.plugin :require_valid_schema
+Sequel::Model.plugin :timestamps, update_on_create: true
 Sequel::Model.plugin :subclasses unless ENV['RACK_ENV'] == 'development'
 
 unless defined?(Unreloader)
